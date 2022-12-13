f1377288d7e7d6f9d7638dd8753defd8d561e11e
Sowry <sowry@rascality.nz>
2022-12-14 11:43:47 +1300

    Add auto slider timeout.

--- main.js
+++ main.js
@@ -382,7 +382,37 @@ class Slider {
       slides: {
         perView: this.getNumberOfSlides,
         spacing: 0
-      }
+      },
+      [
+        (slider) => {
+          let timeout;
+          let mouseOver = false;
+          function clearNextTimeout() {
+            clearTimeout(timeout);
+          }
+          function nextTimeout() {
+            clearTimeout(timeout);
+            if (mouseOver) return
+            timeout = setTimeout(() => {
+              slider.next();
+            }, 2000);
+          }
+          slider.on("created", () => {
+            slider.container.addEventListener("mouseover", () => {
+              mouseOver = true;
+              clearNextTimeout();
+            });
+            slider.container.addEventListener("mouseout", () => {
+              mouseOver = false;
+              nextTimeout();
+            });
+            nextTimeout();
+          });
+          slider.on("dragStarted", clearNextTimeout);
+          slider.on("animationEnded", nextTimeout);
+          slider.on("updated", nextTimeout);
+        }
+      ]
     });
   }
 
